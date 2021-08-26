d0ab990c32b66358b2d25e30e631ccc4f156b573
heitoranjos15 <heitoranjos96@gmail.com>
2021-08-26 00:17:28 -0300

    initial commit

---
+++ dashboard/socialmedia/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from pymongo import MongoClient
+
+client = MongoClient('3.134.245.186', 27017)
+
+db = client.sponsorlytix
+
+# Create your views here.
+def facebook(request):
+    posts = list(db.social_media_posts.find())
+    print(posts)
+    data = { 'd': posts}
+    return render(request, 'index.html', data)
+
+def twitter(request):
+    posts = list(db.twitter_metrics.find())
+    print(posts[0])
+    data = { 'd': posts}
+    return render(request, 'twitter.html', data)
+
